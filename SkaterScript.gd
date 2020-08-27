7c9fe161f61f68ce4a33f8063f523a412d6db00a
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-08-27 02:00:32 +0100

    :construction: add camera zooming

--- SkaterScript.gd
+++ SkaterScript.gd
@@ -4,11 +4,28 @@ var grindVelocityMagnitude := 0
 var grindingRail = null
 var regularVelocity := Vector2(0,0)
 var crouched := false
+var camera : Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for child in get_children():
+		if(child is Camera2D):
+			camera = child
 	pass # Replace with function body.
-	
+
+func _process(delta):
+	var lower_lim = 0.6
+	var upper_lim = 1.0
+	var ratio = abs(regularVelocity.x/350)
+	var x_zoom : float
+	if(ratio < lower_lim):
+		x_zoom = lower_lim
+	elif(ratio > upper_lim):
+		x_zoom = upper_lim
+	else:
+		x_zoom = ratio
+	camera.zoom = Vector2(x_zoom,x_zoom)
+
 func _physics_process(delta):
 	if(grindingRail):
 		grinding_movement(delta)
