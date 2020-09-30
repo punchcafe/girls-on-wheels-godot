49fd797ec3c9f90bdfe9d255503abf95c43d72a4
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-09-30 18:04:53 +0100

    :sparkles: add gravity and rail friction

--- scripts/skater/movement_strategies/grind_movement_strategy.gd
+++ scripts/skater/movement_strategies/grind_movement_strategy.gd
@@ -3,6 +3,8 @@ extends "res://scripts/skater/movement_strategy.gd"
 const SkaterStates = preload("res://scripts/skater/SkaterStates.gd")
 const rail_mount_utility = preload("res://scripts/utils/rail_mount_utility.gd")
 
+const mu_coefficient = 0.5
+const gravity = Vector2(0, -9.18)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,6 +16,15 @@ func _ready():
 
 
 func move(skater, delta):
+	
+	var velocity_projected_to_rail = rail_mount_utility.get_velocity_component_in_rail_direction(skater.regularVelocity, skater._state_manager.get_grinding_rail().get_rail_angle())
+	var friction = velocity_projected_to_rail*mu_coefficient*-1
+	var gravity_in_direction_of_rail = rail_mount_utility.get_velocity_component_in_rail_direction(gravity, skater._state_manager.get_grinding_rail().get_rail_angle())
+	
+	var vel_delta = (friction + gravity_in_direction_of_rail) * delta
+	
+	skater.regularVelocity = skater.regularVelocity + vel_delta
+	
 	if(skater.regularVelocity.angle_to(skater._state_manager.get_grinding_rail().get_rail_angle()) != 0
 	|| skater.regularVelocity.angle_to(skater._state_manager.get_grinding_rail().get_rail_angle()) != 180):
 		skater.regularVelocity = rail_mount_utility.get_velocity_component_in_rail_direction(skater.regularVelocity, skater._state_manager.get_grinding_rail().get_rail_angle())
